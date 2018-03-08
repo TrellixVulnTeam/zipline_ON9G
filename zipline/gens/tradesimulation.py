12537ab2651bda5b0fad950d68136ed07d35fd04
gyunt <truth0233@gmail.com>
2018-03-09 05:07:31 +0900

    use standard python 'contextlib'.

--- zipline/gens/tradesimulation.py
+++ zipline/gens/tradesimulation.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from contextlib2 import ExitStack
+from contextlib import ExitStack
 from copy import copy
 from logbook import Logger, Processor
 from pandas.tslib import normalize_date
